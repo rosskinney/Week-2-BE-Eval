eddbb750066b6a5112f3affcdbf2d92336fe0e50
MakerSquare Student <students@themakersquare.com>
2013-11-22 11:56:33 -0600

    Add code for sets the opponent for each player

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -12,7 +12,10 @@ describe Tennis::Game do
       expect(game.player2).to be_a(Tennis::Player)
     end
 
-    it 'sets the opponent for each player'
+    it 'sets the opponent for each player' do
+      expect(game.player1.opponent).to eq(game.player2)
+      expect(game.player2.opponent).to eq(game.player1)
+    end
   end
 
   describe '#wins_ball' do
